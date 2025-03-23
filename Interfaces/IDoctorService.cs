d3ec86044c355a84350f97e0640bc521e9f46c7c
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-23 21:02:46 +0500

    Implemented DoctorService

---
+++ Interfaces/IDoctorService.cs
@@ -0,0 +1,15 @@
+﻿using DocBookAPI.Models;
+
+namespace DocBookAPI.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<Doctor> GetDoctorByIdAsync(int id);
+        Task<Doctor> GetDoctorByEmailAsync(string email);
+        Task<Doctor> GetDoctorByUserNameAsync(string userName);
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor> AddDoctorAsync(Doctor doctor);
+        Task<Doctor> UpdateDoctorAsync(Doctor doctor);
+        Task<bool> DeleteDoctorAsync(int id);
+    }
+}
---
+++ Services/DoctorService.cs
@@ -0,0 +1,64 @@
+﻿using DocBookAPI.Data;
+using DocBookAPI.Interfaces;
+using DocBookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocBookAPI.Services
+{
+    public class DoctorService : IDoctorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DoctorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Doctor> AddDoctorAsync(Doctor doctor)
+        {
+            await _context.Doctors.AddAsync(doctor);
+            await _context.SaveChangesAsync();
+            return doctor;
+        }
+
+        public async Task<bool> DeleteDoctorAsync(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync()
+        {
+            return await _context.Doctors.ToListAsync();
+        }
+
+        public async Task<Doctor> GetDoctorByEmailAsync(string email)
+        {
+            return (await _context.Doctors.FirstOrDefaultAsync(d => d.User.Email == email))!;
+        }
+
+        public async Task<Doctor> GetDoctorByIdAsync(int id)
+        {
+            return (await _context.Doctors.FindAsync(id))!;
+        }
+
+        public async Task<Doctor> GetDoctorByUserNameAsync(string userName)
+        {
+            return (await _context.Doctors.FirstOrDefaultAsync(d => d.User.UserName == userName))!;
+        }
+
+        public async Task<Doctor> UpdateDoctorAsync(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            await _context.SaveChangesAsync();
+            return doctor;
+        }
+    }
+}
