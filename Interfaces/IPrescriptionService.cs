b88112ea8bb844021a9a7e3987b874141db915e7
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-24 23:35:36 +0500

    Implemeted PrescriptionService

---
+++ Interfaces/IPrescriptionService.cs
@@ -0,0 +1,18 @@
+﻿using DocBookAPI.Models;
+
+namespace DocBookAPI.Interfaces
+{
+    public interface IPrescriptionService
+    {
+        Task<Prescription> CreatePrescription(Prescription prescription);
+        Task<Prescription> GetPrescription(int id);
+        Task<IEnumerable<Prescription>> GetPrescriptions();
+        Task<bool> UpdatePrescription(Prescription prescription);
+        Task<bool> DeletePrescription(int id);
+        Task<IEnumerable<Prescription>> GetPrescriptionsByAppointmentId(int appointmentId);
+        Task<IEnumerable<Prescription>> GetPrescriptionsByPatientId(int patientId);
+        Task<IEnumerable<Prescription>> GetPrescriptionsByDoctorId(int doctorId);
+        Task<IEnumerable<Prescription>> GetPrescriptionsByDoctorIdAndPatientId(int doctorId, int patientId);
+        Task<Prescription> GetPrescriptionByDoctorIdAndPatientIdAndAppointmentId(int doctorId, int patientId, int appointmentId);
+    }
+}
---
+++ Services/PrescriptionService.cs
@@ -0,0 +1,84 @@
+﻿using DocBookAPI.Data;
+using DocBookAPI.Interfaces;
+using DocBookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocBookAPI.Services
+{
+    public class PrescriptionService : IPrescriptionService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public PrescriptionService(ApplicationDbContext context, ILogger<PrescriptionService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Prescription> CreatePrescription(Prescription prescription)
+        {
+            _context.Prescriptions.Add(prescription);
+            await _context.SaveChangesAsync();
+            return prescription;
+        }
+
+        public async Task<Prescription> GetPrescription(int id)
+        {
+            return (await _context.Prescriptions.FindAsync(id))!;
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptions()
+        {
+            return await _context.Prescriptions.ToListAsync();
+        }
+
+        public async Task<bool> UpdatePrescription(Prescription prescription)
+        {
+            if (await GetPrescription(prescription.Id) == null)
+            {
+                return false;
+            }
+            _context.Prescriptions.Update(prescription);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeletePrescription(int id)
+        {
+            var prescription = await GetPrescription(id);
+            if (prescription != null)
+            {
+                _context.Prescriptions.Remove(prescription);
+                await _context.SaveChangesAsync();
+            }
+
+            return prescription != null;
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsByAppointmentId(int appointmentId)
+        {
+            return await _context.Prescriptions.Where(p => p.AppointmentId == appointmentId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsByPatientId(int patientId)
+        {
+            return await _context.Prescriptions.Where(p => p.Appointment.PatientId == patientId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsByDoctorId(int doctorId)
+        {
+            return await _context.Prescriptions.Where(p => p.Appointment.DoctorId == doctorId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsByDoctorIdAndPatientId(int doctorId, int patientId)
+        {
+            return await _context.Prescriptions.Where(p => p.Appointment.DoctorId == doctorId && p.Appointment.PatientId == patientId).ToListAsync();
+        }
+
+        public async Task<Prescription> GetPrescriptionByDoctorIdAndPatientIdAndAppointmentId(int doctorId, int patientId, int appointmentId)
+        {
+            return (await _context.Prescriptions.FirstOrDefaultAsync(p => p.Appointment.DoctorId == doctorId && p.Appointment.PatientId == patientId && p.AppointmentId == appointmentId))!;
+        }
+    }
+}
